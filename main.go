5c49789167f12fee0597b660621f4dfc5767815f
matta9001 <matta9001@gmail.com>
2022-10-03 20:07:59 -0500

    Finished very simple app to retrieve bitcoin data
    Simply retrieves the bitcoin ticker data from `api.blockchain.com`, parses the JSON, then prints the 24h price.

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("hello world")
+	resp, err := http.Get("https://api.blockchain.com/v3/exchange/tickers/BTC-USD")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var btc map[string]interface{}
+	json.Unmarshal([]byte(body), &btc)
+
+	fmt.Println(btc["price_24h"])
 }
